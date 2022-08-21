16351299e319559adf4c0507f76add95c0db28e5
MariaMagdalina <metodist_rmmc@mail.ru>
2022-08-21 17:02:08 +0300

    задание 4

---
+++ task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Данная программа сравнивает три числа и находит среди них максимальное.
+Console.WriteLine("Введите первое число ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+double n3 = Convert.ToDouble(Console.ReadLine());
+double max = n1;
+if(n2 > max) { max = n2; }
+if(n3 > max) { max = n3; }
+Console.WriteLine("максимальное число: " + max);
