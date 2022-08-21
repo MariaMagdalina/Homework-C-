5c9a7de3ed640fb65956da0e0c20dc899c2ca66e
MariaMagdalina <metodist_rmmc@mail.ru>
2022-08-21 17:38:17 +0300

    задание 8

---
+++ task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// данная программа принимает на вход число n и показывает все четные числа от 1 до n
+Console.Write("Введите целое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if(n > 1)
+{
+   while(count <= n)
+   { Console.WriteLine(count); count +=2;}
+}
+else Console.WriteLine("В данном интервале четных чисел нет");
+
